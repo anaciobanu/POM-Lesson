0286c19a35df2e49987c66acc3c0bc301501b49e
Ana Ciobanu <anaciobanu1604@gmail.com>
2022-05-24 13:08:38 -0400

    Add pipeline attachment

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,12 @@ from selenium import webdriver
 from datetime import datetime
 from utilities.ReadConfig import ReadConfig
 from utilities.Logger import Logger
+import os 
 
 logger = Logger.get_logger()
 
 @pytest.fixture()
-def setup(request):
+def setup(request, add_pipelines_attachment):
     # the following code runs before each test
     url = ReadConfig.get_app_url()
     driver = webdriver.Firefox()
@@ -18,8 +19,10 @@ def setup(request):
     
     # the following code runs after each test
     def teardown():
-        image_name = fr".\screenshots\image-{datetime.today().strftime('%m%d%y-%H%M%S')}.png"
+        image_name = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+        fr".\screenshots\image-{datetime.today().strftime('%m%d%y-%H%M%S')}.png")
         driver.save_screenshot(image_name)
+        add_pipelines_attachment(image_name, "testing SwagLabs webpage")
         logger.info(f'Taking a screenshot {image_name}')
         logger.info(f'Closing the browser')
         driver.quit()
